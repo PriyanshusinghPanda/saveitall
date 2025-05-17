5601d5a867cf7daa24bcd29226b5dab5fbe432ab
Priyanshu Singh <priyanshusingh@50-50-53-148.snpr.wi.frontiernet.net>
2025-05-17 18:21:44 +0530

    starting on the frontend

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,11 @@
-import {exec} from 'child_process';
+// import {exec} from 'child_process';
 import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
 
+const app = express()
+
+
+
+app.listen(3000)
